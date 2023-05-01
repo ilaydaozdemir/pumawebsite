7881d3b65b6e168074a0e6c3847c45bd8980cdb5
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2023-05-01 22:27:44 +0300

    ADD : Global component for header

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import styled from "styled-components";
+import HeaderPumaLogo from "../assets/HeaderPumaLogo.png";
+import CartLogo from "../assets/cart.png";
+const Header = (props) => {
+  return (
+    <div>
+      <Container>
+        <IconContainer>
+          <MenuRoundedIcon />
+        </IconContainer>
+        <div>
+          <img src={HeaderPumaLogo} width="82px" height="42px" />
+        </div>
+        <IconContainer>
+          <img src={CartLogo} width="36px" height="42px" />
+        </IconContainer>
+      </Container>
+      <HeaderText>EXCLUSIVE</HeaderText>
+    </div>
+  );
+};
+export default Header;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-top: 28px;
+  @media (min-width: 768px) {
+  }
+  @media (min-width: 1024px) {
+  }
+`;
+export const IconContainer = styled.div`
+  margin-top: 9px;
+`;
+export const HeaderText = styled.div`
+  margin-top: 10px;
+  text-align: center;
+  font-weight: 100;
+  font-size: 10px;
+  letter-spacing: 2.6px;
+  opacity: 0.8;
+`;
